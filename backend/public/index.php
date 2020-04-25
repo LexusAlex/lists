96f6ac3f943d8ba3cc20bcb80c95e669fe1a27d6
alex <alexsey_89@bk.ru>
2020-04-25 22:36:44 +0300

    function buildtree

--- backend/public/index.php
+++ backend/public/index.php
@@ -29,17 +29,10 @@ $db = new \Lists\MariaDb('mysql:dbname=lists;host=backend-mariadb;port=3306', 'r
 //$db->createTestData();
 echo '<pre>';
 
-/*
-$new = array();
-foreach ($db->all() as $a){
-    $new[$a['parent_id']][] = $a;
-}
-$tree = $tree->createTree($new, $new[1]); // changed
-print_r($tree);
-*/
 
-//print_r($tree->build_tree($db->all(),'parent_id', 'id'));
 
-print_r($tree->reduce(function ($acc, $n){ return $n;}, $tree->build_tree($db->all(),'parent_id', 'id'),[]));
+print_r($tree->buildTree($db->all()));
 
-print_r($db->all());
+//print_r($tree->reduce(function ($acc, $n){ return $n;}, $tree->buildTree($db->all()), []));
+
+//print_r($db->all());
--- backend/src/Tree.php
+++ backend/src/Tree.php
@@ -11,7 +11,7 @@ class Tree
      * @param int $parent_id
      * @return array
      */
-    public function flatToTree(array $flat_array, int $parent_id = 0) {
+    public function flatToTree1(array $flat_array, int $parent_id = 0) {
 
         $combine_array = array_combine(array_column($flat_array, 'id'), array_values($flat_array));
 
@@ -28,54 +28,60 @@ class Tree
         });
     }
 
-    public function flatToTree2(array $flat_array, $parent_id = 0, &$result = []) {
-
-        /*
-        return array_map(function (&$key ,$item) {
-            $key = $item['id'];
-            return $item;
-        },array_keys($flat_array),$flat_array);
-        */
-
-        /*
-        return array_reduce($flat_array, function ($acc, &$item){
-
-            return $acc;
-        },[]);
-        */
-    }
-
-    public function createTree(&$list, $parent){
+    /**
+     * $new = array();
+        foreach ($db->all() as $a){
+        $new[$a['parent_id']][] = $a;
+        }
+        $t = $tree->flatToTree2($new, $new[1]);
+     * @param $list
+     * @param $parent
+     * @return array
+     */
+    public function flatToTree2(&$list, $parent){
         $tree = array();
         foreach ($parent as $k => $l){
             if(isset($list[$l['id']])){
-                $l['children'] = $this->createTree($list, $list[$l['id']]);
+                $l['children'] = $this->flatToTree2($list, $list[$l['id']]);
             }
             $tree[] = $l;
         }
         return $tree;
     }
 
-    public function build_tree($flat, $pidKey, $idKey = null)
+    public function buildTree(array $flat,
+                              string $pidKey = 'parent_id',
+                              string $idKey = 'id',
+                              string $sibKey = 'children',
+                              string $typeKey = 'type',
+                              $parent_id_start = null
+    ) : array
     {
-        $grouped = array();
-        foreach ($flat as $sub) {
-            $grouped[$sub[$pidKey]][] = $sub;
+        // Группируем по родительским элементам
+        // при необходимости в качестве ключей можно выставить id элемента в качестве ключа $sub['id']
+        $grouped = [];
+        foreach ($flat as $value) {
+            $grouped[$value[$pidKey]][] = $value;
         }
 
-        $fnBuilder = function ($siblings) use (&$fnBuilder, $grouped, $idKey) {
+        // Воспользуемся функцией высшего порядка вызывая ее на каждом элементе, которая будет рекурсивной
+        $fnBuilder = function ($siblings) use (&$fnBuilder, $grouped, $idKey, $sibKey, $typeKey) {
             foreach ($siblings as $k => $sibling) {
                 $id = $sibling[$idKey];
                 if (isset($grouped[$id])) {
-                    $sibling['children'] = $fnBuilder($grouped[$id]);
+                    $sibling[$sibKey] = $fnBuilder($grouped[$id]);
+                } else {
+                    if ($sibling[$typeKey] == 0) {
+                        $sibling[$sibKey] = [];
+                    }
                 }
                 $siblings[$k] = $sibling;
             }
             return $siblings;
         };
-        $tree = $fnBuilder($grouped[null]);
-        return $tree;
-    } // end of build_tree.
+        // Вызываем функцию с нужного ключа
+        return $fnBuilder($grouped[$parent_id_start]);
+    }
 
     function reduce($func, $tree, $accumulator)
     {
